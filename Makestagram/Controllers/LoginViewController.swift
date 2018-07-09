f2ab5295cebfc6d227ed11e22eefa526458c6d21
Arya Gharib <aryakhan81@icloud.com>
2018-07-09 16:58:58 -0700

    Tutorial Update 1

---
+++ Makestagram/Controllers/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  Makestagram
+//
+//  Created by Arya Gharib on 7/9/18.
+//  Copyright © 2018 Sina Gharib. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        
+    }
+}
